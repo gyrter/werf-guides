7792666b7a57c16de8a59e72ffae5a2a015c0069
Nikolay Bogdanov <nikolay.bogdanov@flant.com>
2021-02-19 11:17:01 +0300

    ++

---
+++ examples/nodejs/290_local_dev/migrations/1613722416000_001-labels.js
@@ -0,0 +1,14 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.createTable('labels', {
+        id: 'id',
+        label: { type: 'varchar(255)', notNull: true, unique: true }
+    )
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('labels')
+};
